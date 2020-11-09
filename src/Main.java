cadba8fb36de3b6203d09cb49d6023b03ec36efb
KRL1991 <kimx4706@edu.easj.dk>
2020-11-09 10:43:46 +0100

    Added more Frugt, Grønsager og kød samt strings grøntsager

--- src/Main.java
+++ src/Main.java
@@ -16,24 +16,28 @@ public class Main {
         a.holdbarhed = new Date(1608768000);
         a.smidUd();
         a.moden = true;
+        System.out.println(a.type);
         a.coolDown();
 
         Banana b = new Banana();
         b.stykpris = 4;
         b.moden = true;
         b.holdbarhed = new Date(1608768000);
+        System.out.println(b.sort);
         b.coolDown();
 
         Grape g = new Grape();
         g.stykpris = 1;
         g.moden = true;
         g.holdbarhed = new Date(1608768000);
+        System.out.println(g.type);
         g.coolDown();
 
         Strawberry s = new Strawberry();
         s.stykpris = 3;
         s.moden = true;
         s.holdbarhed = new Date(1608768000);
+        System.out.println(s.sort);
         s.coolDown();
 
 
@@ -47,30 +51,35 @@ public class Main {
         Leek l = new Leek();
         l.stykpris = 5;
         l.holdbarhed = new Date(1608768000);
+        System.out.println(l.type);
         l.freeze();
 
         Peas p = new Peas();
         p.stykpris = 0.1;
         p.holdbarhed = new Date(1608768000);
-        p.freeze();
         System.out.println(p.type);
+        p.freeze();
+
 
         Beef beef = new Beef();
         beef.stykpris = 30;
         beef.holdbarhed = new Date(1608768000);
         beef.weight = 200;
+        System.out.println(beef.type);
         beef.coolDown();
 
         Lam lam = new Lam();
         lam.stykpris = 45;
         lam.holdbarhed = new Date(1608768000);
         lam.weight = 150;
+        System.out.println(lam.type);
         lam.coolDown();
 
         Pork pork = new Pork();
         pork.stykpris = 35;
         pork.holdbarhed = new Date(1608768000);
         pork.weight = 250;
+        System.out.println(pork.type);
         pork.coolDown();
 
 
